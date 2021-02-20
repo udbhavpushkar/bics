7b258fa76e0a8e8ab1addf3b292e5568b7e78d2d
udbhavpushkar <udbhavpushkar@gmail.com>
2021-02-20 13:14:50 +0530

    1st day commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import './App.css';
+import "materialize-css/dist/js/materialize"
 import "materialize-css/dist/css/materialize.css"
 import {Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
-import React from "react";
+import React, {useEffect} from "react";
 import logo from "../../logo.png"
 import Home from "../Home";
 import Courses from "../Courses";
@@ -10,30 +11,63 @@ import Certificate from "../Certificate";
 import About from "../About";
 import NotFound from "../NotFound";
 import {Col, Row} from "react-materialize";
+import Materialize from "materialize-css"
 
 function App() {
+
+    const routes = [{link: "/", name: "Home", component: Home},
+        {link: "/courses", name: "Courses", component: Courses},
+        {link: "/gallery", name: "Gallery", component: Gallery},
+        {link: "/certificate", name: "Certificate Verification", component: Certificate},
+        {link: "/about", name: "About Us", component: About},
+
+    ]
   return (
       <Router>
           <Row>
-            <Col l={2} m={12} s={12}>
+            <Col l={2} m={12} s={12} className="center">
                 <img style={{"width": "140px", "height": "140px"}} src={logo} alt="logo"/>
             </Col>
-              <Col l={10} m={12} s={12} className="hide-on-small-only">
-                  <h4 style={{"margin-top": "10px", "font-weight": "900"}}
+              <Col l={10} m={12} s={12} className="">
+                  <h4 style={{"marginTop": "10px", "fontWeight": "900"}}
                       className="red-text text-darken-4 center">
                       Bharat Institute Of Computer Science
                   </h4>
+                  <h6 className="center">A unit of Mercy Foundation Regd. No 05398</h6>
+                  <h6 className="center"><small>Govt of Uttar Pradesh</small></h6>
+                  <h6 className="center">An ISO certified institute</h6>
               </Col>
           </Row>
-          <Link to="/gallery">Gallery</Link>
+          <nav className="red darken-3 accent-4" role="navigation">
+              <div className="nav-wrapper right" style={{"width": "90% !important"}}>
+                  <ul className="left hide-on-med-and-down">
+                      {routes.map((l, index)=>(
+                          <li key={index.toString()}><Link to={l.link}>{l.name}</Link></li>
+                      ))}
+                  </ul>
+                  <ul id="nav-mobile" className="sidenav">
+                      {routes.map((l, index)=>(
+                          <li key={index}><Link to={l.link}>{l.name}</Link></li>
+                      ))}
+                  </ul>
+                  <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+              </div>
+          </nav>
           <Switch>
-              <Route path="/" exact component={Home}/>
-              <Route path="/courses" component={Courses}/>
-              <Route path="/about" component={About}/>
-              <Route path="/verify-certificate" component={Certificate}/>
-              <Route path="/gallery" component={Gallery}/>
+              {routes.map((r)=>(
+                  <Route path={r.link} exact component={r.component}/>
+              ))}
               <Route component={NotFound}/>
           </Switch>
+          <footer className="page-footer red darken-3 accent-4">
+              <div className="footer-copyright">
+                  <div className="container">
+                      <p>
+                          <span className="yellow-text text-lighten-3">BICS</span> <i className="material-icons">copyright</i>
+                      </p>
+                  </div>
+              </div>
+          </footer>
       </Router>
   );
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Col, Row} from "react-materialize";
+import Materialize from "materialize-css";
+
 
 const Home = () => {
+    useEffect(()=>{
+        Materialize.AutoInit()
+    })
     return (
-        <div>
-            
-        </div>
+        <Row>
+            <div className="col m9 s12 push-m3" style={{"padding": "0"}}>
+                <div className="carousel carousel-slider">
+                    <a key="1" className="carousel-item">
+                        <img alt="img2"
+                             style={{"height": "400px"}}
+                             src="https://images.unsplash.com/photo-1571260899304-425eee4c7efc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"/>
+                        </a>
+                    <a key="2" className="carousel-item">
+                        <img alt="img3"
+                             style={{"height": "400px"}}
+                             src="https://images.unsplash.com/photo-1531482615713-2afd69097998?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"/>
+                    </a>
+                    <a key="3" className="carousel-item">
+                        <img alt="img3"
+                             style={{"height": "400px"}}
+                             src="https://images.unsplash.com/photo-1576267423445-b2e0074d68a4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"/>
+                    </a>
+                </div>
+            </div>
+            <div className="col m3 s12 pull-m9">
+                <div className="center" style={{"padding": "10px 5px"}}>Notice</div>
+            </div>
+        </Row>
     );
 };
 
